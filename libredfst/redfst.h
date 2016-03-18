552b1b081b404c0d3437e4f496165bba38374483
Lucas Schnorr <schnorr@inf.ufrgs.br>
2016-03-18 12:09:49 -0300

    declaring public functions in redfst.h

--- libredfst/redfst.h
+++ libredfst/redfst.h
@@ -9,6 +9,9 @@ void redfst_monitor_set_status(int n);
 #ifndef REDFSTLIB_STATIC
 void redfst_region(int id);
 void redfst_region_all(int id);
+void redfst_reset(void);
+void redfst_print(void);
+void redfst_exit(void);
 #else
 #include "redfst/region.c"
 #endif
