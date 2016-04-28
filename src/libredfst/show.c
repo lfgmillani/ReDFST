322f731aba6c1e11a5c6d3ba82cdf4c7497396e0
Alef Farah <afarah1@users.noreply.github.com>
2016-04-28 13:00:55 -0300

    Avoid segfault on failure to open

--- src/libredfst/show.c
+++ src/libredfst/show.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "libredfst_config.h"
 #include "region.h"
 #include "global.h"
@@ -13,6 +14,10 @@ redfst_show(){
   int cpu;
   int region;
   fp = fopen("redfst.profile","wt");
+  if (!fp) {
+    fprintf(stderr, "failed to open redfst.profile\n");
+    exit(1);
+  }
   for(region=0;region<REDFSTLIB_MAX_REGIONS;++region){
     totTime = totRef = totMiss = 0;
     for(cpu=0;cpu<REDFSTLIB_MAX_THREADS;++cpu){
